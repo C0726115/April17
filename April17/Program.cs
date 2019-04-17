0a8b315c6c713c95de7fee95331cc88591dd1090
visualstudio <visualstudio@122-PC36>
2019-04-17 08:47:51 -0400

    second

--- April17/Program.cs
+++ April17/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +10,28 @@ namespace April17
     {
         static void Main(string[] args)
         {
+            new GenerateList().Run(57);
+        }
+
+    }
+    class GenerateList
+    {
+        const int LEN = 100;
+        ArrayList listQ;
+        public void Run(int AddUpTonumbers)
+        {
+            Random r = new Random();
+
+            listQ = new ArrayList();
+            for (int i = 0; i < LEN; i++) { listQ.Add(r.Next(100));
+ }
+            this.findTwoNumbersThatAddUpTo(AddUpTonumbers);
+        }
+
+        public void findTwoNumbersThatAddUpTo(int addUpTonumbers)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
