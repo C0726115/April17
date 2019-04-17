241d63dd09bc62d5530d66c072b879e49c04bba2
visualstudio <visualstudio@122-PC36>
2019-04-17 09:05:47 -0400

    third commit

--- April17/Program.cs
+++ April17/Program.cs
@@ -6,32 +6,47 @@ using System.Threading.Tasks;
 
 namespace April17
 {
-    class Program
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ConsoleApp1
     {
-        static void Main(string[] args)
+        class Program
         {
-            new GenerateList().Run(57);
-        }
+            static void Main(string[] args)
+            {
+                new GenerateList().Run(57);
+            }
+            class GenerateList
+            {
 
+                const int LEN = 100;
+                ArrayList ListQ;
+                public void Run(int AddUpToNumber)
+
+                {
+                    Random r = new Random();
+                    ListQ = new ArrayList();
+                    for (int i = 0; i < LEN; i++) { ListQ.Add(r.Next(100)); }
+                    this.findTwoNumbersThatAddUpTo(AddUpToNumber);
+                }
+
+                private void findTwoNumbersThatAddUpTo(int addUpToNumber)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            public void findTwoNumbersThatAddUpTo(int addUpToNumber)
+            {
+
+            }
+
+
+
+        }
     }
-    class GenerateList
-    {
-        const int LEN = 100;
-        ArrayList listQ;
-        public void Run(int AddUpTonumbers)
-        {
-            Random r = new Random();
-
-            listQ = new ArrayList();
-            for (int i = 0; i < LEN; i++) { listQ.Add(r.Next(100));
- }
-            this.findTwoNumbersThatAddUpTo(AddUpTonumbers);
-        }
-
-        public void findTwoNumbersThatAddUpTo(int addUpTonumbers)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
 }
